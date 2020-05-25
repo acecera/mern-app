81f0be7c5e1cdb25d50d05e92b6833cc2c7f1158
Anthony Cera <ace_cera@yahoo.com>
2020-05-24 22:42:18 -0700

    added a card element to make the user item component look nicer.

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,7 +7,7 @@ const Users = () => {
         {
             id: '11', 
             name: 'Ace', 
-            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fman-avatar-profile-vector-21372076&psig=AOvVaw0YY2Lbu5Ju_sHhTaY1qItJ&ust=1590456860823000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjn5dLvzekCFQAAAAAdAAAAABAD',
+            image: 'https://www.facebook.com/photo/?fbid=10202068541053357&set=a.1608690335449',
             places: 3
         }
     ];
