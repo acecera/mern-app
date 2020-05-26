ec62b6dfd567604c1a0e5baaa84fb896859c7587
Anthony Cera <ace_cera@yahoo.com>
2020-05-25 23:05:06 -0700

    added the syntax for PlaceList and Userplaces and implemented them into App.js.

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'a1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://en.wikipedia.org/wiki/File:Empire_State_Building_(aerial_view).jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'p1'
+    },
+    {
+        id: 'c1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://en.wikipedia.org/wiki/File:Empire_State_Building_(aerial_view).jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'p2'
+    }
+];
+
 const UserPlaces = () => {
-    return 
+    return <PlaceList items={DUMMY_PLACES} />;
 };
 
 export default UserPlaces;
