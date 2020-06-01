9a93e99c71232d8c65802e5ae6f56b9ecf73e7ff
Anthony Cera <ace_cera@yahoo.com>
2020-05-31 23:29:07 -0700

    added logic for new place page as well as the styling for new place. Added in input element and styling for the input to search for new places.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlaces.css'
+
 const NewPlace = () => {
-    return <h2>NewPlace Works!</h2>
-}
+    return (
+        <form className="place-form">
+            <Input type="text" label="Title"  />
+        </form>
+    );
+};
 
 export default NewPlace;
