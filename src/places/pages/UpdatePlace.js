2118adf6944e6c264875e48c0933bbeff5392864
Anthony Cera <ace_cera@yahoo.com>
2020-06-02 16:58:10 -0700

    added input and button componenet logic with validation.

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+
 const DUMMY_PLACES = [
     {
         id: 'a1',
@@ -42,7 +46,33 @@ const UpdatePlace = () => {
     }
 
     return (
-        <h2>Update Place</h2>
+        <form>
+            <Input 
+                id="title" 
+                element="input" 
+                type="text" 
+                label="Title" 
+                validators={[VALIDATOR_REQUIRE()]} 
+                errorText="Please enter a valid title"
+                onInput={() => {}}
+                value={identifiedPlace.title}
+                valid={true}
+            />
+            <Input 
+                id="description" 
+                element="textarea" 
+                type="text" 
+                label="Description" 
+                validators={[VALIDATOR_MINLENGTH(5)]} 
+                errorText="Please enter a valid description"
+                onInput={() => {}}
+                value={identifiedPlace.description}
+                valid={true}
+            />
+            <Button type="submit" disabled={true}>
+                UPDATE PLACE
+            </Button>
+        </form>
     );
 };
 
