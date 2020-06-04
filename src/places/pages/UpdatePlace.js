ab0dbd0932c9ca1f821b1389227bbaa342eef6ad
Anthony Cera <ace_cera@yahoo.com>
2020-06-03 23:24:12 -0700

    imported auth page into app.js. Added delete handlers for places pages as well as buttons and modal for when you press the delete button.

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -13,7 +13,7 @@ const DUMMY_PLACES = [
         id: 'a1',
         title: 'Empire State Building',
         description: 'One of the most famous sky scrapers in the world',
-        imageUrl: 'https://en.wikipedia.org/wiki/File:Empire_State_Building_(aerial_view).jpg',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
         address: '20 W 34th St, New York, NY 10001',
         location: {
             lat: 40.7484405,
