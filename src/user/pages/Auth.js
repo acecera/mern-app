ca3247e203e04220a8602eb65f8d0b534f86494a
Anthony Cera <ace_cera@yahoo.com>
2020-06-17 21:49:51 -0700

    added a imageupload.js adn css file to add a profile image when you signup. Imported that imageupload component to the Auth.js file.

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
@@ -35,7 +36,8 @@ const Auth = () => {
             setFormData(
                 {
                     ...formState.inputs,
-                    name: undefined
+                    name: undefined,
+                    image: undefined
                 }, 
                 formState.inputs.email.isValid && formState.inputs.password.isValid
             );
@@ -45,6 +47,10 @@ const Auth = () => {
                  ...formState.inputs,
                  name: {
                      value: '',
+                     isValid: false
+                 },
+                 image: {
+                     value: null,
                      isValid: false
                  }
                 }, 
@@ -56,6 +62,8 @@ const Auth = () => {
 
     const authSubmitHandler = async event => {
         event.preventDefault();
+
+        console.log(formState.inputs);
 
         if (isLoginMode) {
         try {
@@ -107,6 +115,7 @@ const Auth = () => {
                         onInput={inputHandler}
                     />
                 )}
+                {!isLoginMode && <ImageUpload center id="image" onInput={inputHandler} />}
                     <Input 
                         element="input" 
                         id="email" 
